967fb28bc5ad1b5303054f8745abe0546b6042c4
latarniakonto <wisniewskikq27@gmail.com>
2022-01-09 15:20:14 +0100

    Created simple movement system

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Client : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private float speed = 3f;
     void Start()
     {
         
@@ -13,6 +14,16 @@ public class Client : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        float xTranslation = Input.GetAxis("Horizontal");
+        float yTranslation = Input.GetAxis("Vertical");
+        xTranslation *= speed * Time.deltaTime;
+        yTranslation *= speed * Time.deltaTime;
+
+        transform.Translate(xTranslation, yTranslation, 0f);
     }
 }
